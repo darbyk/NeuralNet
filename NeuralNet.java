c539fafa2138fe84cbb1cd4d06119386c9615de1
darbyk <dhk5t@virginia.edu>
2017-02-12 16:46:14 -0500

    hi
    hi

--- NeuralNet.java
+++ NeuralNet.java
@@ -5,7 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-
+//hi
 public class NeuralNet {
 	//LBFGS variables
 	int iflag[] = {0};
